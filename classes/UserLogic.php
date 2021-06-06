516e900a796956aaabc1d980a894ba46f4b0aee7
daisuketerashita <ekusiad.tera@icloud.com>
2021-06-06 14:22:05 +0900

    ログインチェック機能とリダイレクト処理

--- classes/UserLogic.php
+++ classes/UserLogic.php
@@ -81,9 +81,24 @@ class UserLogic{
             return $user;
         }catch(\Exception $e){
             return false;
+        }  
+    }
+
+
+    /**
+     * ログインチェック
+     * @param void
+     * @return bool $result
+    */
+    public static function checkLogin(){
+        $result = false;
+
+        //セッションにログインユーザーが入っていなかったらfalse
+        if(isset($_SESSION['login_user']) && $_SESSION['login_user']['id'] > 0){
+            return $result = true;
         }
 
-        
+        return $result;
     }
 }
 ?>
